57d4fc1d9ffdf1f24a417ecff08ae1a641d93fda
maryamrz402 <136293878+maryamrz402@users.noreply.github.com>
2023-10-29 12:54:59 +0330

    created search.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.css';
 import Weather from "./Weather";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>weather search</h1>
-        <Weather city="paris"/>
+        <Search/>
+        
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='container'>
+    <App /></div>
   </React.StrictMode>
 );
 
